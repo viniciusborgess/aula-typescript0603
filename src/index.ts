fd62e78eb5212706b907758d661bea40e6ebf851
Vinicius Borges <viniciussobreiraborges@hotmail.com>
2025-03-13 08:30:50 -0300

    Implementando exercício 04

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ console.log(multiplicacao(5,2))
 
 //Exercicio 03
  
- function inverterArray<T>(array: T[]): T[] {
+ /*function inverterArray<T>(array: T[]): T[] {
      return array.slice().reverse();
  }
  const numeros: number[] = [1,2,3,4,5];
@@ -32,3 +32,48 @@ console.log(multiplicacao(5,2))
  
  const palavras: string[] = ["Olá", "Mundo", "TypeScript"];
  console.log(inverterArray(palavras)); // Resultado esperado ["typescript", "mundo", "olá"]
+
+ interface Repositorio<T> {
+     salvar(dado: T): void;
+     obterTodos(): T[];
+ }
+ class UsuarioRepositorio implements Repositorio<{ nome: string; email: string }> {
+     private usuarios: { nome: string; email: string }[] = [];
+     salvar(dado: { nome: string; email: string }): void {
+         this.usuarios.push(dado);
+     }
+     obterTodos(): { nome: string; email: string }[] {
+         return this.usuarios;
+     }
+ }
+ // Teste da implementação
+ const repo = new UsuarioRepositorio();
+ repo.salvar({ nome: "João", email: "joao@email.com" });
+ repo.salvar({ nome: "Maria", email: "maria@email.com" });
+ console.log("Lista de usuários:", repo.obterTodos());*/
+
+ 
+
+ //Exercicio 04
+
+ interface Repositorio<T> {
+    salvar(dado: T): void;
+    obterTodos(): T[];
+}
+class UsuarioRepositorio implements Repositorio<{ nome: string; email: string }> {
+    private usuarios: { nome: string; email: string }[] = [];
+    salvar(dado: { nome: string; email: string }): void {
+        this.usuarios.push(dado);
+    }
+    obterTodos(): { nome: string; email: string }[] {
+        return this.usuarios;
+    }
+}
+// Teste da implementação
+const repo = new UsuarioRepositorio();
+repo.salvar({ nome: "João", email: "joao@email.com" });
+repo.salvar({ nome: "Maria", email: "maria@email.com" });
+console.log("Lista de usuários:", repo.obterTodos());
+
+
+ 
